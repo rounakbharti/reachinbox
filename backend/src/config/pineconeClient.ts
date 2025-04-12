4be7ebd44c2f38b651f2788b7f80c4e7fd0a9816
rounakbharti <167201531+rounakbharti@users.noreply.github.com>
2025-04-13 00:33:53 +0530

    refactored the code

--- backend/src/config/pineconeClient.ts
+++
@@ -1,8 +0,0 @@
-// src/config/pineconeClient.ts
-import { Pinecone } from '@pinecone-database/pinecone';
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const pinecone = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY!,
-});
